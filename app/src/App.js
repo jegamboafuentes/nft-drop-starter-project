af631acaebaad3b59144fc6762d59fc1dafe2e67
Jorge Enrique Gamboa Fuentes <snake2209@hotmail.com>
2022-05-14 00:07:42 -0400

    end of Create your Mint NFT button, successfully working

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import CandyMachine from './CandyMachine';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -68,11 +69,12 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🍭 Candy Drop</p>
+          <p className="header">🍭 JEGF Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
-          {/* Add the condition to show this only if we don't have a wallet address */}
           {!walletAddress && renderNotConnectedContainer()}
         </div>
+        {/* Check for walletAddress and then pass in walletAddress */}
+      {walletAddress && <CandyMachine walletAddress={window.solana} />}
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
@@ -84,6 +86,7 @@ const App = () => {
         </div>
       </div>
     </div>
+  
   );
 };
 
